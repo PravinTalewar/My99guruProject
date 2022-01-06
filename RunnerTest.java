982f5f7183d2af2ef1a553dbc512f6d9f325d45b
PravinTalewar <88527485+PravinTalewar@users.noreply.github.com>
2022-01-06 22:35:17 +0530

    Add files via upload

---
+++ RunnerTest.java
@@ -0,0 +1,27 @@
+package Runer;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "Stepdef",
+        tags = "",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+
+        dryRun = false
+)
+
+public class RunnerTest extends AbstractTestNGCucumberTests {
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
+
+
